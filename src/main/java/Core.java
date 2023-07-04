3ba2f342150715d10f09e26366083b99db3f47a6
Bruno Guerra <g.g.bruno.00@gmail.com>
2023-07-04 16:02:23 -0300

    Simplifying to make it more readable

--- src/main/java/Core.java
+++ src/main/java/Core.java
@@ -1,20 +1,16 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Core {
 
     public static double computeIndexOfCoincidence(byte[] textBytes) {
-        HashMap<Character, Long> charOccurrences = new HashMap<>();
         long textSize = textBytes.length;
-        for (byte b : textBytes) {
-            char letter = (char) b;
-            charOccurrences.merge(letter, 1L, Long::sum);
-        }
-        charOccurrences.remove('\n');
-        return charOccurrences.entrySet().stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue() * (e.getValue() - 1)))
-            .values().stream().reduce(Long::sum).map(value -> {
+        var charOccurrences = new String(textBytes).chars()
+                .mapToObj(c -> (char) c)
+                .filter(c -> c != '\n')
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        return charOccurrences.values().stream()
+            .map(value -> value * (value - 1))
+            .reduce(Long::sum).map(value -> {
                 long denominator = textSize * (textSize - 1);
                 return ((double) value / denominator);
             }).orElseThrow(RuntimeException::new);
