3fab74188ac457d44e518d48f016e6c4f33fb455
Bruno Guerra <g.g.bruno.00@gmail.com>
2023-07-05 17:20:36 -0300

    Using Function for inner computation

--- src/main/java/Core.java
+++ src/main/java/Core.java
@@ -1,3 +1,4 @@
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Core {
@@ -10,9 +11,15 @@ public class Core {
                 .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
         return charOccurrences.values().stream()
             .map(value -> value * (value - 1))
-            .reduce(Long::sum).map(value -> {
-                long denominator = textSize * (textSize - 1);
-                return ((double) value / denominator);
-            }).orElseThrow(RuntimeException::new);
+            .reduce(Long::sum)
+            .map(computeValue(textSize))
+            .orElseThrow(RuntimeException::new);
+    }
+
+    private static Function<Long, Double> computeValue(long textSize) {
+        return value -> {
+            long denominator = textSize * (textSize - 1);
+            return ((double) value / denominator);
+        };
     }
 }
